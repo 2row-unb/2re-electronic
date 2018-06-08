71804a8d9952ae3fcec2f706c76142bb4571b5ba
Lucianaaf <lucianaaf12@gmail.com>
2018-06-08 14:54:57 -0300

    Update kernel.py

--- kernel.py
+++ kernel.py
@@ -13,6 +13,8 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
 
+indice = 0
+
 class Kernel(gabby.Gabby):
     def transform(self, message):
         logging.debug(
@@ -37,40 +39,36 @@ class Kernel(gabby.Gabby):
 	ty  = time.time() - tx
 
 	#print ty
+	
         return ty
 
 
     def get_buttons(self):
-        
-        #Verifica quantas vezes o botao foi acionado
-        #contador = 0
-	button _incr = 0
-	button_decr = 0
-	button_reset = 0
+        button_reset = 0
 	
         #Define o pino do botao como entrada
         GPIO.setup(18, GPIO.IN)
         GPIO.setup(11, GPIO.IN)
-        GPIO.setup(16, GPIO.IN)
+        GPIO.setup(12, GPIO.IN)
         
-	time.sleep(0.3)
     	if GPIO.input(18) == True:
-		contador += 1
-		button_incr = contador
-       
-        if GPIO.input(11) == True:
-		contador += 1
-		button_decr = contador
-		
-        if GPIO.input(17) == True:
-		
+        	time.sleep(0.3)
+		indice += 1
+		    
+	if GPIO.input(11) == True:
+		time.sleep(0.3)
+        	indice -= 1
+		    
+	if GPIO.input(12) == True:
 		button_reset = 1
-		contador += 1
-		if (contador == 2):
-			button_reset = 0
-			
+        	
         
-        return [button_incr, button_decr, button_reset]
+	#return [indice, button_reset]"
+	#print "inidice:" +str(indice)
+	#print "reset:" +str(button_reset)
+	#time.sleep(1)
+			
+        return [indice, button_reset]
 
 
     def get_weight(self):
